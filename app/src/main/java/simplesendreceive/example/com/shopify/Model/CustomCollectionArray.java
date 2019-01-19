26fd8b139de6c346be716bb7b23109441f0aca65
coded_ <selasikudolo@cmail.carleton.ca>
2019-01-18 23:33:44 -0500

    added new fonts and colours

--- app/src/main/java/simplesendreceive/example/com/shopify/Model/CustomCollectionArray.java
+++ app/src/main/java/simplesendreceive/example/com/shopify/Model/CustomCollectionArray.java
@@ -5,8 +5,6 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-import simplesendreceive.example.com.shopify.Model.CustomCollection;
-
 public class CustomCollectionArray {
 
     @SerializedName("custom_collections")
